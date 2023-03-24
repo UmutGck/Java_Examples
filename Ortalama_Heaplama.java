309e29c90678778c8bd5cc99a9fcf52aaed04615
UmutGck <81230338+UmutGck@users.noreply.github.com>
2023-03-25 00:39:11 +0300

    Create Ortalama_Heaplama.java

---
+++ Ortalama_Heaplama.java
@@ -0,0 +1,32 @@
+// Değişkenleri oluşturma
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        // Scanner sınıfımızı tanımladık
+        Scanner inp = new Scanner(System.in);
+
+        // Kullanıcıdan değerleri al
+        System.out.print("Matematik Notunuz : ");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double ortalama = toplam / 6.0;
+        System.out.print("Ortalamanız : " + ortalama);
+
+        boolean kosul = ortalama >= 60;
+        String sonuc = kosul ? " Sınıfı Geçtin" : " Sınıfta kaldın";
+        System.out.println(sonuc);
